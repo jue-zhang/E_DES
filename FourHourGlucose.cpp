635fe8f58be7e4b961d7ea3cf76489b7bff2d03b
jue-zhang <40574046+jue-zhang@users.noreply.github.com>
2018-06-27 11:34:48 +0800

    add SetSubjectTypeFittedParams()

--- FourHourGlucose.cpp
+++ FourHourGlucose.cpp
@@ -20,16 +20,25 @@ using namespace std;
 int main(int argc, const char * argv[]) {
     
     E_DES eDES;
-    double foodIntake = 75E3; // unit: mg
-    double bodyMass = 75.; // unit: kg
+    double foodIntake = 30E3; // unit: mg
+    double bodyMass = 83.3; // unit: kg
     double Gpl_init = 5.; // unit: mmol/L
     double Ipl_init = 8.; // unit: mU/L
-    // Note: Currently, the model (without manually modifications) predicts glucose values below the initial values 
-    // after about three hours of food intake. This problem is more severe when the amount of foodIntake is large. 
-    // For example, when foodIntake = 1000E3 mg, the final glucose can be around 2~3, which is not acceptable. 
-    // To overcome this, I manually demand that as long as the glucose level is below the initial value, 
-    // the initial value will be used for the following time intervals. 
-    // Therefore, it looks like that the glucose level returns to the initial value eventually.
+    
+    // Set the subject type and fitted parameters
+    // 1. Type of subject: 0 - healthy person, 1 - Type-I diabetes, 2 - Type-II diabetes
+    // 2. Note: Type-I is NOT supported yet, and Type-II parameters are used instead.
+    eDES.SetSubjectTypeFittedParams(2);
+    
+    // Note:
+    // 1. Special treatment for the healty person: Currently, the model (without manually modifications) predicts glucose values
+    //      below the initial values after about three hours of food intake. This problem is more severe when the amount of
+    //      foodIntake is large. For example, when foodIntake = 1000E3 mg, the final glucose can be around 2~3, which is not
+    //      acceptable. To overcome this, I manually demand that as long as the glucose level is below the initial value, the
+    //      initial value will be used for the following time intervals. Therefore, it looks like that the glucose level
+    //      returns to the initial value eventually.
+    // 2. Diabetes: Now the above treatment also applies to the diabetes cases. However, because the glucose level for the diabetes
+    //      subject usually does not return the normal value after a long time period,  the treatment is actually not effective.
     std::vector<double> glucoses = eDES.FourHourGlucose(foodIntake, bodyMass, Gpl_init, Ipl_init);
     cout << "four hour glucose (10-min intervals): " << endl;
     int i = 1;
@@ -40,10 +49,4 @@ int main(int argc, const char * argv[]) {
     
     return 0;
 }
-
-
-
-
-
-
 
